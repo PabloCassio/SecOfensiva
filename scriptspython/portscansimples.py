ac9d65883a212aeaee9689c8578b3d2a29542703
PabloCassio <82393094+PabloCassio@users.noreply.github.com>
2021-11-16 18:43:27 -0300

    Update portscansimples.py

--- scriptspython/portscansimples.py
+++ scriptspython/portscansimples.py
@@ -1,12 +1,13 @@
 import socket
 url = str(input("Digite a url que deseja scannear: "))
-print ("Meu primeiro scanner de porta")
-print (" O alvo será " , url)
+print("Meu primeiro scanner de porta")
+print(" O alvo será ", url)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(2)
-porta=80
-c = s.connect_ex((str(url),int(porta)))
-if c == 0:
-print ("Porta ",n,": Aberta")
-else:
-print ("Porta ",n,": Fechada")
+portas= [80, 8080, 443, 25, 20, 21, 3389, 1433, 3306]
+for num in portas:
+    c = s.connect_ex((str(url), int(num)))
+    if c == 0:
+        print("Porta ",num,": Aberta")
+    else:
+        print("Porta ",num,": Fechada")
